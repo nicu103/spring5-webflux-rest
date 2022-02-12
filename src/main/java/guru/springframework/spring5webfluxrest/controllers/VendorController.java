48d5ce3075d754ef5d1fc816b0fe46f29c05484f
Nicolae Taban <nicolae.taban@gmail.com>
2022-02-12 16:43:47 +0200

    Adds Post endpoint for Vendors

--- src/main/java/guru/springframework/spring5webfluxrest/controllers/VendorController.java
+++ src/main/java/guru/springframework/spring5webfluxrest/controllers/VendorController.java
@@ -2,10 +2,9 @@ package guru.springframework.spring5webfluxrest.controllers;
 
 import guru.springframework.spring5webfluxrest.domain.Vendor;
 import guru.springframework.spring5webfluxrest.repositories.VendorRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -26,6 +25,12 @@ public class VendorController {
     @GetMapping("/{id}")
     Mono<Vendor> getVendorById(@PathVariable("id") String id) {
         return vendorRepository.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    Mono<Void> create(@RequestBody Publisher<Vendor> vendorStream) {
+        return vendorRepository.saveAll(vendorStream).then();
     }
 
 }
--- src/test/java/guru/springframework/spring5webfluxrest/controllers/VendorControllerTest.java
+++ src/test/java/guru/springframework/spring5webfluxrest/controllers/VendorControllerTest.java
@@ -1,17 +1,20 @@
 package guru.springframework.spring5webfluxrest.controllers;
 
+import guru.springframework.spring5webfluxrest.domain.Category;
 import guru.springframework.spring5webfluxrest.domain.Vendor;
 import guru.springframework.spring5webfluxrest.repositories.VendorRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 
 public class VendorControllerTest {
@@ -20,7 +23,7 @@ public class VendorControllerTest {
     VendorController vendorController;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         vendorRepository = Mockito.mock(VendorRepository.class);
         vendorController = new VendorController(vendorRepository);
         webTestClient = WebTestClient.bindToController(vendorController).build();
@@ -58,5 +61,21 @@ public class VendorControllerTest {
                 .expectBody(Vendor.class)
                 .value(hasProperty("firstName", equalTo("Jenny")))
                 .value(hasProperty("lastName", equalTo("Buck")));
+    }
+
+    @Test
+    public void create() {
+        BDDMockito.given(vendorRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Vendor.builder().build()));
+        Mono<Vendor> vendorToSave = Mono.just(Vendor.builder()
+                .firstName("Joe")
+                .lastName("Buck")
+                .build());
+        webTestClient.post()
+                .uri("/api/v1/vendors")
+                .body(vendorToSave, Vendor.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
     }
 }

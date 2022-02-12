6469a516eed6d1d3622ab5efef9f9713d1896f05
Nicolae Taban <nicolae.taban@gmail.com>
2022-02-12 13:39:37 +0200

    Create Category and Vendor Controller Tests

---
+++ src/test/java/guru/springframework/spring5webfluxrest/controllers/VendorControllerTest.java
@@ -0,0 +1,62 @@
+package guru.springframework.spring5webfluxrest.controllers;
+
+import guru.springframework.spring5webfluxrest.domain.Vendor;
+import guru.springframework.spring5webfluxrest.repositories.VendorRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
+import static org.mockito.ArgumentMatchers.anyString;
+
+public class VendorControllerTest {
+    WebTestClient webTestClient;
+    VendorRepository vendorRepository;
+    VendorController vendorController;
+
+    @Before
+    public void setUp() throws Exception {
+        vendorRepository = Mockito.mock(VendorRepository.class);
+        vendorController = new VendorController(vendorRepository);
+        webTestClient = WebTestClient.bindToController(vendorController).build();
+    }
+
+    @Test
+    public void getAllVendors() {
+        BDDMockito.given(vendorRepository.findAll())
+                .willReturn(
+                        Flux.just(
+                                Vendor.builder().firstName("Joe").lastName("Buck").build(),
+                                Vendor.builder().firstName("Jenny").lastName("Buck").build()
+                        )
+                );
+        webTestClient
+                .get()
+                .uri("/api/v1/vendors")
+                .exchange()
+                .expectBodyList(Vendor.class)
+                .hasSize(2);
+    }
+
+    @Test
+    public void getVendorById() {
+        BDDMockito.given((vendorRepository.findById(anyString())))
+                .willReturn(
+                        Mono.just(
+                                Vendor.builder().firstName("Jenny").lastName("Buck").build()
+                        )
+                );
+        webTestClient
+                .get()
+                .uri("/api/v1/vendors/someId")
+                .exchange()
+                .expectBody(Vendor.class)
+                .value(hasProperty("firstName", equalTo("Jenny")))
+                .value(hasProperty("lastName", equalTo("Buck")));
+    }
+}
